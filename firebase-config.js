ec3acde1c9f9c9650c3886724269e55091f0c437
foodservicedpt-stack <foodservicedpt@gmail.com>
2026-07-30 17:43:07 +0200

    Change geminiApiKey in firebase-config.js
    Updated the geminiApiKey in the Firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -11,5 +11,5 @@ const firebaseConfig = {
   storageBucket:     "foodservice-alergenos-5fe9b.firebasestorage.app",
   messagingSenderId: "653432937402",
   appId:             "1:653432937402:web:4b9abbe72ae59d700c5b83",
-  geminiApiKey:      "TU_GEMINI_API_KEY_AQUI"
+  geminiApiKey:      "AQ.Ab8RN6Kdc6-QCITz_VMVaZMQvVYab6raEvAdTSeLlhfTptQMEw"
 };
